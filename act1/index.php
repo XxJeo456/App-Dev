9940d343ef08d73a9407e5d2adfeb10787f39163
Jeoffrey <jeffreyplata086@gmail.com>
2026-05-02 08:17:55 +0800

    First PHP page

---
+++ act1/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Resume </title>
+</head>
+<body>
+    <div class="container">
+        <div class="main-info">
+            <h3>Jeoffre Lee R. Plata</h3>
+        </div>
+    </div>
+</body>
+</html>
